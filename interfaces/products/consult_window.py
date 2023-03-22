e5fb5bd53d266c951af416859239ad86be617f6e
Vitor <84788940+VBSX@users.noreply.github.com>
2023-03-22 16:31:11 -0300

    Update consult_window.py
    searcher working

--- interfaces/products/consult_window.py
+++ interfaces/products/consult_window.py
@@ -1,11 +1,89 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QPushButton, QHBoxLayout, QWidget, QLineEdit,QApplication,QMessageBox
+import sys
+import os
+
+path = os.path.abspath('database/database_manager')
+sys.path.append(path)
+from products_database import *
+
 class ConsultWindow(QWidget):
 
     def __init__(self):
         super().__init__()
-        layout = QVBoxLayout()
-        self.label = QLabel("Another Window")
-        layout.addWidget(self.label)
-        self.setLayout(layout)
+        self.setWindowTitle('Buscar')
+        self.database_handle = ProductsData()
         
+        self.id = QLineEdit()
+        self.id.setPlaceholderText("Id") 
         
+        self.name = QLineEdit()
+        self.name.setPlaceholderText("NOME")
+        
+        self.pesquisar = QPushButton("Pesquisar", clicked=self.search_product)
+        
+        self.layout = QHBoxLayout(self)
+        self.layout.addWidget(self.id)
+        self.layout.addWidget(self.name)
+        self.layout.addWidget(self.pesquisar)
+        
+    
+    def search_product(self):
+        self.verify_what_user_want_to_search()
+    
+    def verify_what_user_want_to_search(self):
+        id_product = self.id.text()
+        name_product = self.name.text()
+        
+        if id_product and name_product:
+            try:
+                data = self.database_handle.get_products_by_id(id_product)
+            except:
+                try:
+                    data = self.database_handle.get_products_by_name(name_product)
+                    
+                except:
+                    self.show_dialog('erro database ao buscar')
+                    print('erro ao tentar buscar por id e por nome')
+            if data:
+                print(data)  
+            else:
+                self.show_dialog('Produto não encontrado')       
+        
+        elif id_product:
+            try:
+                data = self.database_handle.get_products_by_id(id_product)
+            except:
+                self.show_dialog('erro database ao buscar')
+                print('erro ao buscar pelo id')
+            if data:
+                print(data)  
+            else:
+                self.show_dialog('Produto não encontrado')
+            
+            
+        elif name_product:
+            try:
+                data = self.database_handle.get_products_by_name(name_product)
+            except:
+                self.show_dialog('erro database ao buscar')
+                print('erro ao buscar pelo nome')
+            if data:
+                print(data)  
+            else:
+                self.show_dialog('Produto não encontrado')
+        
+        else:
+            self.show_dialog('Coloque uma informação antes de pesquisar!')
+                    
+                    
+    def show_dialog(self, text):
+        QMessageBox.about(self, 'DIALOG', text)
+            
+
+        
+    
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = ConsultWindow()
+    widget.show()
+    sys.exit(app.exec())
