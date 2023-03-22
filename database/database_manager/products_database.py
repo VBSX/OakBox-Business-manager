b6d6c82ccf48a8da2493675617cb5175cbb2fb39
Vitor <84788940+VBSX@users.noreply.github.com>
2023-03-22 16:52:02 -0300

    Update products_database.py
    created function to get data, by name and id + name of product

--- database/database_manager/products_database.py
+++ database/database_manager/products_database.py
@@ -1,5 +1,5 @@
+
 import sqlite3
-import sys
 import os       
 
 
@@ -17,12 +17,26 @@ class ProductsData():
 
         
     def get_products_by_id(self, id_of_product):
+        
         self.cursor.execute(
             f"SELECT Id,Nome,Quantidade, Valor_unitario FROM Produtos WHERE Id = '{id_of_product}'")
         data_database = self.cursor.fetchall()
 
         return data_database
     
+    def get_products_by_name(self, name_of_product):
+        self.cursor.execute(
+            f"SELECT Id,Nome,Quantidade, Valor_unitario FROM Produtos WHERE Nome LIKE '%{name_of_product}%'")
+        data_database = self.cursor.fetchall()
+
+        return data_database
+    
+    def get_product_by_id_and_name(self, id_of_product,name_of_product):
+        self.cursor.execute(
+            f"SELECT Id,Nome,Quantidade, Valor_unitario FROM Produtos WHERE Id = '{id_of_product}'AND Nome LIKE '%{name_of_product}%'")
+        data_database = self.cursor.fetchall()
+
+        return data_database
 if __name__ =='__main__':
     s = ProductsData()
     print(s.get_products_by_id(3))
