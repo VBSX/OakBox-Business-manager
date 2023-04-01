60c5964b00b77a535bc13314c6ab58eae28bdf1a
VBSX <vitorgamer.mais@gmail.com>
2023-03-31 22:00:10 -0300

    Create dialog_window_confirmation.py

---
+++ interfaces/checkout/dialog_window_confirmation.py
@@ -0,0 +1,33 @@
+from PySide6.QtWidgets import QApplication, QDialog, QVBoxLayout, QHBoxLayout, QLabel, QPushButton
+
+class MyDialog(QDialog):
+    def __init__(self, info, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Minha Janela de Diálogo")
+        self.info = info
+        # Cria um QLabel para exibir as informações
+        label = QLabel(self.info)
+        # Cria dois QPushButton, um para Concluir e outro para Cancelar
+        button_box = QHBoxLayout()
+        conclui_button = QPushButton("Concluir Venda")
+        conclui_button.clicked.connect(self.accept)  # Chama o método accept() quando o botão "Concluir" for pressionado
+        cancela_button = QPushButton("Cancelar")
+        cancela_button.clicked.connect(self.reject)  # Chama o método reject() quando o botão "Cancelar" for pressionado
+        button_box.addWidget(conclui_button)
+        button_box.addWidget(cancela_button)
+        # Adiciona o QLabel e os botões ao layout vertical
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        layout.addLayout(button_box)
+        # Define o layout do diálogo
+        self.setLayout(layout)
+
+if __name__ == '__main__':
+    app = QApplication([])
+    info = "Essas são as informações que serão exibidas no diálogo."
+    dialog = MyDialog(info)
+    if dialog.exec() == QDialog.Accepted:
+        print("O botão 'Concluir' foi pressionado.")
+    else:
+        print("O botão 'Cancelar' foi pressionado.")
+    app.exit()
