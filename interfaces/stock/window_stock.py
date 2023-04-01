2f8f2463a5488eb9743add4ce245b28c135ae3e2
VBSX <vitorgamer.mais@gmail.com>
2023-03-31 22:00:47 -0300

    todo

--- interfaces/stock/window_stock.py
+++ interfaces/stock/window_stock.py
@@ -24,53 +24,38 @@ class StockPage(QMainWindow):
         #Add the menu options of the program
         self.config_the_menubar()
         self.config_the_toolbar()
-        
         self.consult_window = ProductsInfo(self)
-        
         self.label_id_description = QLabel()
-        self.label_id_description.setText('ID')
-             
-   
+        self.label_id_description.setText('ID') 
         self.label_id = QLabel()
         self.label_nome_produto = QLabel()
         self.label_nome_produto_description = QLabel()
         self.label_nome_produto_description.setText('PRODUTO')
-        
-        
         self.label_quantidade = QLabel()
         self.label_quantidade_description = QLabel()
         self.label_quantidade_description.setText('QUANTIDADE')
-        
-        
         self.label_quantidade_atual = QLabel()
         self.label_quantidade_atual_description = QLabel()
         self.label_quantidade_atual_description.setText('QUANTIDADE ATUAL')
-        
-        
         self.layout = QGridLayout()
-        
         self.layout.addWidget(self.label_id_description, 0,1)
         self.layout.addWidget(self.label_nome_produto_description,0,2)
         self.layout.addWidget(self.label_quantidade_description,0,3)
         self.layout.addWidget(self.label_quantidade_description,0,4)
         self.layout.addWidget(self.label_quantidade_atual_description,0,5)
-        
         self.layout.addWidget(self.label_id, 1,1)
         self.layout.addWidget(self.label_nome_produto,1,2)
         self.layout.addWidget(self.label_quantidade,1,3)
         self.layout.addWidget(self.label_quantidade,1,4)
         self.layout.addWidget(self.label_quantidade_atual,1,5)
         self.layout.rowStretch(2)
-       
         widget = QWidget()
         widget.setLayout(self.layout)
         self.setCentralWidget(widget)  
 
-    def config_the_menubar(self):
-            
+    def config_the_menubar(self): 
         button_action_logoff = QAction("logoff", self)
         button_action_logoff.triggered.connect(self.logoff)
-        
         bar=self.menuBar()
         file=bar.addMenu('File')
         file.addAction('self.teste')
@@ -89,21 +74,15 @@ class StockPage(QMainWindow):
         self.addToolBar(tool)
         tool.addAction(button_consult_product)
         
-        
     def consult_product(self):
         self.consult_window.show()
     
     def show_products(self, data):
         str_of_products = self.get_id_and_name_of_list(data)
-    
         number_of_products_retorned = self.cont_products(data)
-        
         if number_of_products_retorned >1:
-            self.show_dialog(f'Foi retornado mais de um produto: \n {str_of_products}')
-            
-            
+            self.show_dialog(f'Foi retornado mais de um produto: \n {str_of_products}') 
         else:
-
             print(data[0][1])
             self.id_produto = data[0][0]
             self.nome = data[0][1]
@@ -113,11 +92,9 @@ class StockPage(QMainWindow):
             self.label_nome_produto.setText(str(self.nome))
             self.label_quantidade.setText(str(quantidade))
             self.label_quantidade_atual.setText(str(self.quantidade_atual))
-            
             self.button_atualizar_estoque = QPushButton("Dar entrada no estoque", clicked=self.atualizar_estoque)
             self.layout.addWidget(self.button_atualizar_estoque, 2,5)
 
-        
     def cont_products(self, list):
         number_of_products = 0
         for l in list:
@@ -131,7 +108,6 @@ class StockPage(QMainWindow):
             id_prod = str(product[0])
             name_prod = str(product[1])
             string = string+ '\nid: '+ id_prod+'\nnome: '+ name_prod +'\n'
-
         return string
             
     def atualizar_estoque(self):
@@ -142,8 +118,9 @@ class StockPage(QMainWindow):
         QMessageBox.about(self, 'DIALOG', text)
    
 if  __name__ == "__main__":
-    
     app = QtWidgets.QApplication([])
     widget = StockPage()
     widget.show()
     sys.exit(app.exec())
+#TODO arrumar a janela quando se termina de adicionar o produto no estoque, a janela permanece aberta, e a anterior nao aparece
+#os produtos atualizados.
